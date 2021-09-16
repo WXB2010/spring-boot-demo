7a45991700bf40aca7ab99e4854fa033483b35b4
o_wangxianbing <wangxb2010@qq.com>
2021-09-16 14:59:42 +0800

    sharding-jdbc

---
+++ spring-boot-wxb-shardingjdbc/src/main/java/com/wxb/springbootwxbshardingjdbc/SpringBootWxbShardingjdbcApplication.java
@@ -0,0 +1,13 @@
+package com.wxb.springbootwxbshardingjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWxbShardingjdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWxbShardingjdbcApplication.class, args);
+    }
+
+}
---
+++ spring-boot-wxb-shardingjdbc/src/test/java/com/wxb/springbootwxbshardingjdbc/SpringBootWxbShardingjdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxb.springbootwxbshardingjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWxbShardingjdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
